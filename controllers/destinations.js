a4d0a8907cd8f05b51b98c381d70911e378475d7
jsohnfile <hoodieee@Jaes-MacBook-Air.local>
2020-05-23 13:02:03 -0600

    Add console.log in controller to show flight id

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -6,7 +6,7 @@ module.exports = {
 
 function create(req, res){
     Flight.findById(req.params.id, function(err, flight){
-        console.log(movie, ' this is the movie document')
+        console.log('flight ID: ', flight._id)
         flight.destinations.push(req.body);
         flight.save(function(err){
             res.redirect(`/flights/${flight._id}`)
