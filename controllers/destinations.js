7d96850d9692360789a310649d08610c01c18097
jsohnfile <hoodieee@Jaes-MacBook-Air.local>
2020-05-23 12:53:40 -0600

    Remove unneccessary console.log

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -5,13 +5,9 @@ module.exports = {
 }
 
 function create(req, res){
-    console.log(req.body, ' info on creating the review')
-    console.log(req.params.id, ' movie id that we are writing the review for')
-    // First step is we have to find the movie we want to add the
-    // review too
     Flight.findById(req.params.id, function(err, flight){
         console.log(movie, ' this is the movie document')
-        flight.destinations.push(req.body); // add the review to the reviews array
+        flight.destinations.push(req.body);
         flight.save(function(err){
             res.redirect(`/flights/${flight._id}`)
         })
